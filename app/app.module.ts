6395209749b3e9161cee87aae4b99d8b80478273
Luis Vasconcellos <luis@luisvasconcellos.com>
2016-10-17 18:00:53 -0200

    Change from ‘FIRST’ to ‘SECOND’ angular app

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ]
+  imports: [ BrowserModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
