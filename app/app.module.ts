60549e1436d831b311f7a0348f85832b671e604f
Luis Vasconcellos <luis@luisvasconcellos.com>
2016-10-19 16:11:44 -0200

    WIP

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,18 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+
+import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule
   ],
+  bootstrap: [ AppComponent ],
+
+  providers: [ HeroService ],
+
   declarations: [
     AppComponent,
+    HeroesComponent,
+    DashboardComponent,
     HeroDetailComponent
   ],
-  bootstrap: [ AppComponent ]
 })
 
 export class AppModule { }
